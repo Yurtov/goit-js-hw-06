caea2e4885a4934c7e2d000c6d3a9a8e2dc8db2e
Yurtov <123323348+Yurtov@users.noreply.github.com>
2023-06-04 19:52:07 +0300

    Update task-06.js

--- goit-js-hw-06/src/js/task-06.js
+++ goit-js-hw-06/src/js/task-06.js
@@ -3,7 +3,7 @@ const inputEl = document.querySelector('input');
 inputEl.addEventListener('blur', inFocusChange);
 
 function inFocusChange (){
-    inputEl.value.length === Number(inputEl.dataset.length) ? inputEl.classList.add('valid') : inputEl.classList.add('invalid');
+    const inputTrim = inputEl.value.trim();
+    inputTrim.length === Number(inputEl.dataset.length) ? inputEl.classList.add('valid') : inputEl.classList.add('invalid');
 };
-
 
